a5d87e96cfa1a816a06a4b906278845d54b8e2c1
Nigo <nigo@kimo.com>
2020-06-30 16:14:41 +0800

    Updates

---
+++ precache-manifest.72ef91d42be9675149ff2d9449aa2840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28223dbcf1ceabcba6117e56e50b8bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee3dc437e5c3f98d78e",
+    "url": "/static/css/main.54ab10a3.chunk.css"
+  },
+  {
+    "revision": "cf744c300b91fb8cc7ea",
+    "url": "/static/js/2.a33c39d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a33c39d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee3dc437e5c3f98d78e",
+    "url": "/static/js/main.86f406f2.chunk.js"
+  },
+  {
+    "revision": "6d092129dbb85b7b8948",
+    "url": "/static/js/runtime-main.4664abaa.js"
+  }
+]);
